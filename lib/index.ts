dd676a6a316d75ae270170508bdadee21b1e8ee8
ttay <ttay@umich.edu>
2021-06-17 09:51:06 -0700

    use esbuild

--- lib/index.ts
+++ lib/index.ts
@@ -3,10 +3,9 @@
  */
 
 import Writer from "./writer";
-import CbWriter from "./cb_writer";
 import NopWriter from "./nop_writer";
 import StreamWriter from "./stream_writer";
 import * as Types from "./types";
 import { default as Emitter } from "./emit";
 
-export { Writer, CbWriter, NopWriter, StreamWriter, Types, Emitter };
+export { Writer, NopWriter, StreamWriter, Types, Emitter };
